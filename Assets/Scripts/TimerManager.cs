6fff22dcb924b525ed962ad1182eab2384bbb527
george-593 <63100333+george-593@users.noreply.github.com>
2025-10-19 09:32:55 +0100

    Pass time through scenes

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimerManager : MonoBehaviour
 {
@@ -8,20 +9,26 @@ public class TimerManager : MonoBehaviour
     [SerializeField] private TMP_Text winElapsedTime;
     [SerializeField] private TMP_Text winCurrentTime;
 
-    [Header("Time Settings")]
-    // Total elapsed time from the last level (0 if first level)
-    [SerializeField] private float previousTime;
-    [SerializeField] private float currentTime;
+    static private float previousTime;
+    private float currentTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Null value checks
         if (!timerHUDText || !winElapsedTime || !winCurrentTime)
         {
             Debug.LogError("Required fields are not set!");
             enabled = false;
             return;
         }
+
+        // Reset previous time if we're on level1
+        if (SceneManager.GetActiveScene().name == "Level1")
+        {
+            previousTime = 0f;
+        }
+        Debug.Log(previousTime);
     }
 
     // Update is called once per frame
