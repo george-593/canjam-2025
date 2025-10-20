a6a7bcea29f3b41c58d962314e80048da1c08559
george-593 <63100333+george-593@users.noreply.github.com>
2025-10-20 20:24:35 +0100

    Scalable UI on Instructions

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -5,6 +5,6 @@ public class Startup : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Screen.SetResolution(1920, 1080, false);   
+        Screen.SetResolution(1920, 1080, false);
     }
 }
